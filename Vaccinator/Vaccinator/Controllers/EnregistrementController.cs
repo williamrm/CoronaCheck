a68ab1bbe7f5f0872158ede0517aea21c25cde41
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-02-13 17:47:24 +0100

    2sd commit (app v2)

--- Vaccinator/Vaccinator/Controllers/EnregistrementController.cs
+++ Vaccinator/Vaccinator/Controllers/EnregistrementController.cs
@@ -3,14 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
 
 namespace Vaccinator.Controllers
 {
     public class EnregistrementController : Controller
     {
-        public IActionResult Index()
+        // 
+        // GET: /Enregistrement/
+
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
         }
+
+        // 
+        // GET: /Enregistrement/bienvenue/ 
+
+        public string bienvenue(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+
     }
 }
