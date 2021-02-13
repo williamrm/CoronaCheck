52168445d8f53feeb0321d5fb11f5f8ae0712c94
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-02-13 16:13:28 +0100

    first commit

---
+++ Vaccinator/Models/Data/MvcTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vaccinator.Models;
+
+namespace MvcTest.Data
+{
+    public class MvcTestContext : DbContext
+    {
+        public MvcTestContext(DbContextOptions<MvcTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<test> test { get; set; }
+    }
+}
